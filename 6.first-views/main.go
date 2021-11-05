c45b0ab265f72bf38eefea3959acebfb894f1d95
Gyen Abubakar Sadick <gyen@walulel.com>
2021-11-05 22:17:48 +0000

    implemented custom type for views

--- 6.first-views/main.go
+++ 6.first-views/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
+	"first-views/views"
 	"fmt"
-	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 var (
-	homeTemplate    *template.Template
-	contactTemplate *template.Template
+	homeView    *views.View
+	contactView *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	if err := homeView.Template.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	if err := contactView.Template.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
@@ -36,23 +36,8 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var err error
-
-	homeTemplate, err = template.ParseFiles(
-		"views/index.gohtml",
-		"views/components/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	contactTemplate, err = template.ParseFiles(
-		"views/contact.gohtml",
-		"views/components/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
+	homeView = views.NewView("views/index.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
---
+++ 6.first-views/views/views.go
@@ -0,0 +1,17 @@
+package views
+
+import "html/template"
+
+func NewView(files ...string) *View {
+	files = append(files, "views/components/footer.gohtml")
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View{Template: t}
+}
+
+type View struct {
+	Template *template.Template
+}
