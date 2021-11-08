e231bb82b3bc352a70f401a19fa2fc79a06df6d6
Gyen Abubakar Sadick <gyen@walulel.com>
2021-11-08 21:06:41 +0000

    remove unnecessary utils.go

--- 7.signup-page/main.go
+++ 7.signup-page/main.go
@@ -28,8 +28,11 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
+
 	_, err := fmt.Fprintf(w, "<h1>Couldn't find the path \"%s\".</h1>", path)
-	HandleError(err, "Couldn't process request. :(")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
